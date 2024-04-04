145a1ea1daa416ab81bd557afaf96e96b8b2f91d
hoanthui <hoanthui123@gmail.com>
2024-04-05 00:41:44 +0700

    feat: load posts, friends, notification, friend request to home, handle like/unlike post

--- src/main/java/com/ht/services/PostService.java
+++ src/main/java/com/ht/services/PostService.java
@@ -1,6 +1,8 @@
 package com.ht.services;
 
+import com.ht.entities.Like;
 import com.ht.entities.Post;
+import com.ht.entities.User;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -28,5 +31,41 @@ public class PostService {
         """);
         query.setParameter("userId", userId);
         return query.list();
+    }
+
+    public void like(Long userId, Long postId) throws Exception {
+        Session session = sessionFactory.getCurrentSession();
+
+        Query checkLikedQuery = session.createQuery("""
+            select count(*) from Like where user.id = :userId and post.id = :postId
+        """);
+        checkLikedQuery.setParameter("userId", userId);
+        checkLikedQuery.setParameter("postId", postId);
+        Long count = (Long) checkLikedQuery.uniqueResult();
+
+        if (count > 0) {
+            throw new Exception("Bạn đã thích bài viết này");
+        }
+
+        Like like = new Like();
+        var user = new User();
+        user.setId(userId);
+        var post = new Post();
+        post.setId(postId);
+        like.setUser(user);
+        like.setPost(post);
+        like.setCreatedAt(new Date());
+
+        session.save(like);
+    }
+
+    public void unlike(Long userId, Long postId) {
+        Session session = sessionFactory.getCurrentSession();
+        Query query = session.createQuery("""
+            delete from Like where user.id = :userId and post.id = :postId
+        """);
+        query.setParameter("userId", userId);
+        query.setParameter("postId", postId);
+        query.executeUpdate();
     }
 }
